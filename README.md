3313c7ecfc1feff3922a07abd1f330271fd84e57
tserrania <tserrania@gmail.com>
2020-02-20 00:17:41 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # MediaWeb-J2E
-Logiciel de gestion interne de bibliothèque
+Logiciel de gestion interne de bibliothèque.
+
+## Terminé
+
+- Découpage en paquetages (services, mediatheque, persistantData)
+
+## À faire
+
+- Intégrer le JDBC
+- Système de connexion
+- Liste d'adresses IP
+- Requêtes documents pour la base de données (Ajouter, Récupérer, Mettre à jour)
+- Requêtes utlisateurs pour la base de données
+
+### Abonné
+
+- Emprunter un document
+- Rendre un document
+
+### Bibliothécaire
+
+- Ajouter un document
---
+++ src/persistance/MediathequeData.java
@@ -0,0 +1,48 @@
+package persistantdata;
+
+import java.util.List;
+
+import mediatek2020.*;
+import mediatek2020.items.*;
+
+// classe mono-instance  dont l'unique instance est injectée dans Mediatheque
+// via une auto-déclaration dans son bloc static
+
+public class MediathequeData implements PersistentMediatheque {
+// Jean-François Brette 01/01/2018
+	static {
+		Mediatheque.getInstance().setData(new MediathequeData());
+	}
+
+	private MediathequeData() {
+	}
+
+	// renvoie la liste de tous les documents de la bibliothèque
+	@Override
+	public List<Document> tousLesDocuments() {
+		return null;
+	}
+
+	// va récupérer le User dans la BD et le renvoie
+	// si pas trouvé, renvoie null
+	@Override
+	public Utilisateur getUser(String login, String password) {
+		return null;
+	}
+
+	// va récupérer le document de numéro numDocument dans la BD
+	// et le renvoie
+	// si pas trouvé, renvoie null
+	@Override
+	public Document getDocument(int numDocument) {
+		return null;
+	}
+
+	@Override
+	public void nouveauDocument(int type, Object... args) {
+		// args[0] -> le titre
+		// args [1] --> l'auteur
+		// etc...
+	}
+
+}
